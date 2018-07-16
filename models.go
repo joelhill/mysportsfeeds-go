5c471863907b48a824b63f2fe554d709db05db61
Joel Hill <joel@invisionapp.com>
2018-07-16 14:44:52 -0700

    added seasonal player gamelogs.

--- models.go
+++ models.go
@@ -3,12 +3,18 @@ package msf
 // Main Models
 type GamesIO struct {
 	LastUpdatedOn string      `json:"lastUpdatedOn"`
-	Games         []Game      `json:"games"`
+	Games         *[]Game     `json:"games"`
 	References    *References `json:"references"`
 }
 
 type DfsIO struct {
 	LastUpdatedOn string      `json:"lastUpdatedOn"`
+	References    *References `json:"references"`
+}
+
+type GameLogIO struct {
+	LastUpdatedOn string      `json:"lastUpdatedOn"`
+	GameLog       *[]GameLog  `json:"gamelog"`
 	References    *References `json:"references"`
 }
 
@@ -67,8 +73,11 @@ type Inning struct {
 }
 
 type References struct {
-	TeamReferences  []TeamReference  `json:"teamReferences"`
-	VenueReferences []VenueReference `json:"venueReferences"`
+	TeamReferences       []TeamReference        `json:"teamReferences"`
+	VenueReferences      []VenueReference       `json:"venueReferences"`
+	GameReferences       *[]GameReference       `json:"gameReferences"`
+	PlayerReferences     *[]PlayerReference     `json:"playerReferences"`
+	PlayerStatReferences *[]PlayerStatReference `json:"playerStatReferences"`
 }
 
 type TeamReference struct {
@@ -89,4 +98,162 @@ type VenueReference struct {
 	Name    string `json:"name"`
 	City    string `json:"city"`
 	Country string `json:"country"`
+}
+
+type GameReference struct {
+	ID                       int      `json:"id"`
+	StartTime                string   `json:"startTime"`
+	AwayTeam                 AwayTeam `json:"awayTeam"`
+	HomeTeam                 HomeTeam `json:"homeTeam"`
+	Venue                    Venue    `json:"venue"`
+	VenueAllegiance          string   `json:"venueAllegiance"`
+	ScheduleStatus           string   `json:"scheduleStatus"`
+	OriginalStartTime        *string  `json:"originalStartTime"`
+	DelayedOrPostponedReason *string  `json:"delayedOrPostponedReason"`
+	PlayedStatus             *string  `json:"playedStatus"`
+}
+
+type PlayerReference struct {
+	Player
+	CurrentTeam         Team        `json:"currentTeam"`
+	CurrentRosterStatus string      `json:"currentRosterStatus"`
+	CurrentInjury       *string     `json:"currentInjury"`
+	Height              string      `json:"height"`
+	Weight              float64     `json:"weight"`
+	BirthDate           string      `json:"birthDate"`
+	Age                 int         `json:"age"`
+	BirthCity           string      `json:"birthCity"`
+	BirthCountry        string      `json:"birthCountry"`
+	Rookie              bool        `json:"rookie"`
+	College             string      `json:"college"`
+	Twitter             *string     `json:"twitter"`
+	Handedness          *Handedness `json:"handedness"`
+}
+
+type PlayerStatReference struct {
+	Category     string `json:"category"`
+	FullName     string `json:"fullName"`
+	Description  string `json:"description"`
+	Abbreviation string `json:"abbreviation"`
+	Type         string `json:"type"`
+}
+
+type Handedness struct {
+	Bats   string `json:"bats"`
+	Throws string `json:"throws"`
+}
+
+type GameLog struct {
+	Game   GameLogGame `json:"game"`
+	Player Player      `json:"player"`
+	Team   `json:"team"`
+	Stats  `json:""`
+}
+
+type GameLogGame struct {
+	ID                   int    `json:"id"`
+	StartTime            string `json:"startTime"`
+	AwayTeamAbbreviation string `json:"awayTeamAbbreviation"`
+	HomeTeamAbbreviation string `json:"homeTeamAbbreviation"`
+}
+
+type Player struct {
+	ID           int    `json:"id"`
+	FirstName    string `json:"firstName"`
+	LastName     string `json:"lastName"`
+	Position     string `json:"position"`
+	JerseyNumber *int   `json:"jerseyNumber"`
+}
+
+type Team struct {
+	ID           int    `json:"id"`
+	Abbreviation string `json:"abbreviation"`
+}
+
+type Stats struct {
+	Batting       *Batting       `json:"batting"`
+	Pitching      *Pitching      `json:"pitching"`
+	Fielding      *Fielding      `json:"fielding"`
+	Miscellaneous *Miscellaneous `json:"miscellaneous"`
+}
+
+type Batting struct {
+	AtBats                       *int     `json:"atBats"`
+	Runs                         *int     `json:"runs"`
+	Hits                         *int     `json:"hits"`
+	SecondBaseHits               *int     `json:"secondBaseHits"`
+	ThirdBaseHits                *int     `json:"thirdBaseHits"`
+	Homeruns                     *int     `json:"homeruns"`
+	EarnedRuns                   *int     `json:"earnedRuns"`
+	UnearnedRuns                 *int     `json:"unearnedRuns"`
+	RunsBattedIn                 *int     `json:"runsBattedIn"`
+	BatterWalks                  *int     `json:"batterWalks"`
+	BatterSwings                 *int     `json:"batterSwings"`
+	BatterStrikes                *int     `json:"batterStrikes"`
+	BatterStrikesFoul            *int     `json:"batterStrikesFoul"`
+	BatterStrikesMiss            *int     `json:"batterStrikesMiss"`
+	BatterStrikesLooking         *int     `json:"batterStrikesLooking"`
+	BatterTagOuts                *int     `json:"batterTagOuts"`
+	BatterForceOuts              *int     `json:"batterForceOuts"`
+	BatterPutOuts                *int     `json:"batterPutOuts"`
+	BatterGroundBalls            *int     `json:"batterGroundBalls"`
+	BatterFlyBalls               *int     `json:"batterFlyBalls"`
+	BatterLineDrives             *int     `json:"batterLineDrives"`
+	Batter2SeamFastballs         *int     `json:"batter2SeamFastballs"`
+	Batter4SeamFastballs         *int     `json:"batter4SeamFastballs"`
+	BatterCurveballs             *int     `json:"batterCurveballs"`
+	BatterChangeups              *int     `json:"batterChangeups"`
+	BatterCutters                *int     `json:"batterCutters"`
+	BatterSliders                *int     `json:"batterSliders"`
+	BatterSinkers                *int     `json:"batterSinkers"`
+	BatterSplitters              *int     `json:"batterSplitters"`
+	BatterStrikeouts             *int     `json:"batterStrikeouts"`
+	StolenBases                  *int     `json:"stolenBases"`
+	CaughtBaseSteals             *int     `json:"caughtBaseSteals"`
+	BatterStolenBasePct          *float64 `json:"batterStolenBasePct"`
+	BattingAvg                   *float64 `json:"battingAvg"`
+	BatterOnBasePct              *float64 `json:"batterOnBasePct"`
+	BatterSluggingPct            *float64 `json:"batterSluggingPct"`
+	BatterOnBasePlusSluggingPct  *float64 `json:"batterOnBasePlusSluggingPct"`
+	BatterIntentionalWalks       *int     `json:"batterIntentionalWalks"`
+	HitByPitch                   *int     `json:"hitByPitch"`
+	BatterSacrificeBunts         *int     `json:"batterSacrificeBunts"`
+	BatterSacrificeFlies         *int     `json:"batterSacrificeFlies"`
+	TotalBases                   *int     `json:"totalBases"`
+	ExtraBaseHits                *int     `json:"extraBaseHits"`
+	BatterDoublePlays            *int     `json:"batterDoublePlays"`
+	BatterTriplePlays            *int     `json:"batterTriplePlays"`
+	BatterGroundOuts             *int     `json:"batterGroundOuts"`
+	BatterFlyOuts                *int     `json:"batterFlyOuts"`
+	BatterGroundOutToFlyOutRatio *float64 `json:"batterGroundOutToFlyOutRatio"`
+	PitchesFaced                 *int     `json:"pitchesFaced"`
+	PlateAppearances             *int     `json:"plateAppearances"`
+	LeftOnBase                   *int     `json:"leftOnBase"`
+}
+
+type Pitching struct {
+}
+
+type Fielding struct {
+	InningsPlayed             *float64 `json:"inningsPlayed"`
+	TotalChances              *int     `json:"totalChances"`
+	FielderTagOuts            *int     `json:"fielderTagOuts"`
+	FielderForceOuts          *int     `json:"fielderForceOuts"`
+	FielderPutOuts            *int     `json:"fielderPutOuts"`
+	OutsFaced                 *int     `json:"outsFaced"`
+	Assists                   *int     `json:"assists"`
+	Errors                    *int     `json:"errors"`
+	FielderDoublePlays        *int     `json:"fielderDoublePlays"`
+	FielderTriplePlays        *int     `json:"fielderTriplePlays"`
+	FielderStolenBasesAllowed *int     `json:"fielderStolenBasesAllowed"`
+	FielderCaughtStealing     *int     `json:"fielderCaughtStealing"`
+	FielderStolenBasePct      *float64 `json:"fielderStolenBasePct"`
+	PassedBalls               *int     `json:"passedBalls"`
+	FielderWildPitches        *int     `json:"fielderWildPitches"`
+	FieldingPct               *float64 `json:"fieldingPct"`
+	RangeFactor               *float64 `json:"rangeFactor"`
+}
+
+type Miscellaneous struct {
+	GamesStarted *int `json:"gamesStarted"`
 }
---
+++ seasonal_player_gamelogs.go
@@ -0,0 +1,143 @@
+package msf
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	blaster "github.com/joelhill/go-rest-http-blaster"
+	logrus "github.com/sirupsen/logrus"
+)
+
+// SeaonalPlayerGameLogsOptions - Are the options to hit the seasonal games endpoint
+type SeaonalPlayerGameLogsOptions struct {
+	// URL Parts
+	URL     string
+	Version string
+	Sport   string
+	Season  string
+	Format  string
+
+	// Optional URL Params
+	Team     string
+	Player   string
+	Position string
+	Game     string
+	Date     string
+	Stats    string
+	Sort     string
+	Offset   string
+	Limit    string
+	Force    string
+}
+
+// DefaultSeaonalPlayerGameLogsOptions - Returns the default options to hit the seasonal games endpoint
+func DefaultSeaonalPlayerGameLogsOptions() *SeaonalPlayerGameLogsOptions {
+	return &SeaonalPlayerGameLogsOptions{
+		URL:     URL,
+		Version: VersionV2_0,
+		Sport:   SportMLB,
+		Season:  SeasonCurrent,
+		Format:  FormatJSON,
+	}
+}
+
+// SeaonalPlayerGameLogs - hits the https://api.mysportsfeeds.com/v2.0/pull/mlb/{season}/player_gamelogs.{format} endoint
+func (s *Service) SeaonalPlayerGameLogs(c context.Context, options *SeaonalPlayerGameLogsOptions) (GameLogIO, error) {
+	errorPayload := make(map[string]interface{})
+	mapping := GameLogIO{}
+
+	// make sure we have all the required elements to build the full required url string.
+	err := validateSeaonalPlayerGameLogsURI(options)
+	if err != nil {
+		return mapping, err
+	}
+
+	uri := fmt.Sprintf("%s/%s/pull/%s/%s/player_gamelogs.%s?1=1", options.URL, options.Version, options.Sport, options.Season, options.Format)
+
+	if len(options.Team) > 0 {
+		uri = fmt.Sprintf("%s&team=%s", uri, options.Team)
+	}
+
+	if len(options.Player) > 0 {
+		uri = fmt.Sprintf("%s&player=%s", uri, options.Player)
+	}
+
+	if len(options.Position) > 0 {
+		uri = fmt.Sprintf("%s&position=%s", uri, options.Position)
+	}
+
+	if len(options.Game) > 0 {
+		uri = fmt.Sprintf("%s&game=%s", uri, options.Game)
+	}
+
+	if len(options.Date) > 0 {
+		uri = fmt.Sprintf("%s&date=%s", uri, options.Date)
+	}
+
+	if len(options.Stats) > 0 {
+		uri = fmt.Sprintf("%s&stats=%s", uri, options.Stats)
+	}
+
+	if len(options.Sort) > 0 {
+		uri = fmt.Sprintf("%s&sort=%s", uri, options.Sort)
+	}
+
+	if len(options.Offset) > 0 {
+		uri = fmt.Sprintf("%s&offset=%s", uri, options.Offset)
+	}
+
+	if len(options.Limit) > 0 {
+		uri = fmt.Sprintf("%s&limit=%s", uri, options.Limit)
+	}
+
+	if len(options.Force) > 0 {
+		uri = fmt.Sprintf("%s&force=%s", uri, options.Force)
+	}
+
+	s.Logger = s.Logger.WithFields(logrus.Fields{
+		"URI": uri,
+	})
+	s.Logger.Debug("SeaonalPlayerGameLogs API Call")
+
+	// make you a client
+	client, err := blaster.NewClient(uri)
+	if err != nil {
+		s.Logger.Errorf("failed to create a http client: %s", err.Error())
+		return mapping, err
+	}
+
+	client.SetHeader("Authorization", s.Config.Authorization)
+	client.WillSaturateOnError(&errorPayload)
+	client.WillSaturate(&mapping)
+
+	ctx := context.Background()
+	statusCode, err := client.Get(ctx)
+	if err != nil {
+		s.Logger.Errorf("something went wrong making the get request for SeaonalPlayerGameLogs: %s", err.Error())
+		return mapping, err
+	}
+
+	s.Logger.Infof("SeaonalPlayerGameLogs Status Code: %d", statusCode)
+
+	return mapping, nil
+}
+
+func validateSeaonalPlayerGameLogsURI(options *SeaonalPlayerGameLogsOptions) error {
+	if len(options.URL) == 0 {
+		return errors.New("missing required option to build the url: URL")
+	}
+	if len(options.Version) == 0 {
+		return errors.New("missing required option to build the url: Version")
+	}
+	if len(options.Sport) == 0 {
+		return errors.New("missing required option to build the url: Sport")
+	}
+	if len(options.Season) == 0 {
+		return errors.New("missing required option to build the url: Season")
+	}
+	if len(options.Format) == 0 {
+		return errors.New("missing required option to build the url: Format")
+	}
+	return nil
+}
